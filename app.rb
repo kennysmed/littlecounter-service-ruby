8b1bb2e6671bf4fb38fac5ed52385be52316f40e
Adam Sven Johnson <adam@pkqk.net>
2013-11-18 14:57:33 +0000

    routes for all event urls

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+
+post '/cloud-event/register' do
+end
+
+post '/cloud-event/deregister' do
+end
+
+post '/cloud-event/announce' do
+end
+
+post '/cloud-event/add-owner' do
+end
+
+post '/cloud-event/remove-owner' do
+end
+
+post '/device-event/:event_name' do
+end
